15a2cdf710ea69c182cf1d0107886f42e29875ae
xiaohutushen30 <1098281672@qq.com>
2016-11-27 16:51:38 +0800

    fixed

--- mysite/authen/urls.py
+++ mysite/authen/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^index/$','authen.views.index'),
+    url(r'^api/cr_status/$','authen.views.comput_room_status'),
     url(r'^jifang/$', 'authen.views.jifang'),
     # url(r'^delete_uploadfile/$','fontlib.UserAndRole.views.profile_delte'),
 )
